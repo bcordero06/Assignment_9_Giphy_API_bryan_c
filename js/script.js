72f2ded97703c2b68fa7da203a5d19ed16c1fcc0
bcordero06 <bcordero@northpark.edu>
2026-04-14 14:06:52 -0500

    step 7 done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 console.log("script.js loaded");
+
+function getDogFact() {
+  fetch("https://api.giphy.com/v1/gifs/search?api_key=0I5w13WpmNUtuad2jUCwWJzoVanAdta8&q=dogs&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips")
+    .then(response => response.json())  // Covert the response
+    .then(data => { 
+      // Update the DOM with the dog fact
+      const fact = data.facts[0]; 
+      const output = document.getElementById("fact-output"); 
+      output.textContent = fact; 
+    }) 
+    .catch(error => { 
+      // Message to print if there was an error
+      console.error("Error fetching dog fact:", error); 
+    });
+}
+
