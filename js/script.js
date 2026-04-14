756aedbf8d966c69d6db9fb3bf5b7105e58656f0
bcordero06 <bcordero@northpark.edu>
2026-04-14 14:24:59 -0500

    step 8 done

--- js/script.js
+++ js/script.js
@@ -14,4 +14,25 @@ function getDogFact() {
       console.error("Error fetching dog fact:", error); 
     });
 }
+``
+let images = [];
 
+fetch("https://api.giphy.com/v1/gifs/search?api_key=0I5w13WpmNUtuad2jUCwWJzoVanAdta8&q=dogs&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips")
+    .then(response => response.json())
+    .then(data => {
+        images = data.data.map(gif => gif.images.original.url);
+        console.log(images);
+    });
+
+const gifContainer = document.querySelector("#gif-container")
+const gifButton = document.querySelector("#fetch-dog-btn");
+
+gifButton.addEventListener("click", function() {
+    gifContainer.innerHTML = "";
+
+    for(let i = 0; i < images.length; i++){
+        gifContainer.innerHTML +=`
+        <img src="${images[i]}" class = "col-3 mb-3">
+        `;
+    }
+});
